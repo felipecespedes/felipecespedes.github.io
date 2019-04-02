65bfec1957b4ccfa75b865a75fcdc3a8e4d57eab
felipecespedes <viwell2010@hotmail.com>
2019-04-02 01:10:45 -0500

    initial implementation of the web site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import ContactCard from './components/contact-card/ContactCard';
+import LinksCard from './components/links-card/LinksCard';
+import ProjectsCard from './components/projects-card/ProjectsCard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <ContactCard />
+        <LinksCard />
+        <ProjectsCard />
       </div>
     );
   }
