ead22d1e283378c2d49884fed29c07021beb9d43
felipecespedes <viwell2010@hotmail.com>
2019-04-02 01:52:12 -0500

    update live site

---
+++ precache-manifest.1c8745937d5e251f0e424267a59f7763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfca65ceeda5fc456dd9",
+    "url": "/felipecespedes/felipecespedes.github.io/static/js/runtime~main.5031eed5.js"
+  },
+  {
+    "revision": "9b2d532fa3221cdf08a4",
+    "url": "/felipecespedes/felipecespedes.github.io/static/js/main.3aac17ae.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/felipecespedes/felipecespedes.github.io/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "9b2d532fa3221cdf08a4",
+    "url": "/felipecespedes/felipecespedes.github.io/static/css/main.b9c3d77a.chunk.css"
+  },
+  {
+    "revision": "c447071a59eca829e1244597ad2964c2",
+    "url": "/felipecespedes/felipecespedes.github.io/index.html"
+  }
+];
