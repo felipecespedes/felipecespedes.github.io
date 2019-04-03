ace56f2374b85fb090d0822f1eab7fdd32ce9a31
felipecespedes <viwell2010@hotmail.com>
2019-04-03 13:57:15 -0500

    add analytics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import ContactCard from './components/contact-card/ContactCard';
 import LinksCard from './components/links-card/LinksCard';
 import ProjectsCard from './components/projects-card/ProjectsCard';
+import ReactGA from 'react-ga';
 
 class App extends Component {
   render() {
@@ -13,6 +14,28 @@ class App extends Component {
         <LinksCard />
       </div>
     );
+  }
+
+  componentDidMount() {
+    if (this.isInProduction()) {
+      this.initializeReactGA();
+    }
+  }
+
+  initializeReactGA() {
+    ReactGA.initialize('UA-137663539-1');
+    ReactGA.pageview('/homepage');
+  }
+
+  isInProduction() {
+    const productionHostnames = ['felipecespedes.github.io'];
+    const { hostname } = window.location;
+    let isInProduction = false;
+    if (productionHostnames.includes(hostname)) {
+      isInProduction = true;
+    }
+
+    return isInProduction;
   }
 }
 
